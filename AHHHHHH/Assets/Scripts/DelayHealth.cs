abb79d7656a027e9421cc91a6e0a7634efd1516c
Cameron <cjbaker30@yahoo.com.au>
2019-04-02 17:52:36 +1100

    JHFC

---
+++ AHHHHHH/Assets/Scripts/DelayHealth.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelayHealth : MonoBehaviour
+{
+    [Header("Player Health")]
+    public float maxHealth;
+    public float curHealth;
+    public float delayHealth;
+    public float dropSpeed;
+    public float timeDelay, healthDifference;
+    public bool check;
+    public Slider healthBar, delayBar;
+    public Image healthFill, delayFill;
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = Mathf.Clamp01(curHealth / maxHealth);
+        if (delayHealth > curHealth)
+        {
+            timeDelay -= Time.deltaTime;
+            if (timeDelay <= 0)
+            {
+                if (!check)
+                {
+                    healthDifference = delayHealth - curHealth;
+                    check = true;
+                }
+                //dropSpeed = healthDifference / 4;
+                //Debug.Log(dropSpeed * Time.deltaTime);
+                delayHealth -= (healthDifference/dropSpeed) * Time.deltaTime;
+
+            }
+
+        }
+        else
+        {
+            check = false;
+            timeDelay = 1;
+        }
+        delayBar.value = Mathf.Clamp01(delayHealth / maxHealth);
+        HealthController();
+    }
+    void HealthController()
+    {
+        if (curHealth <= 0 && healthFill.enabled)
+        {
+            healthFill.enabled = false;
+        }
+        else if (!healthFill.enabled && curHealth > 0)
+        {
+            healthFill.enabled = true;
+        }
+        if (delayHealth <= 0 && delayFill.enabled)
+        {
+            delayFill.enabled = false;
+        }
+        if (delayHealth < curHealth)
+        {
+            delayFill.enabled = true;
+            delayHealth = curHealth;
+            delayBar.value = healthBar.value;
+        }
+    }
+
+    void DelayedDrop()
+    {
+
+    }
+
+    void PlayerHandler()
+    {
+
+    }
+}
---
+++ AHHHHHH/Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour {
+    public DelayHealth health;
+    public bool attacked, check;
+    public float difference;
+    public Transform curCheckpoint;
+	// Use this for initialization
+	void Start () {
+        health = GetComponent<DelayHealth>();
+        
+	}
+
+    private void Update()
+    {
+        if (attacked)
+        {
+            if (!check)
+            {
+                difference = health.curHealth - 25;
+                check = true;
+            }
+            if (health.curHealth > difference)
+            {
+                health.curHealth -= 25 * Time.deltaTime;
+            }
+            else
+            {
+                attacked = false;
+                check = false;
+            }
+        }
+        if (health.curHealth <=0 )
+        {
+            gameObject.transform.position = curCheckpoint.position;
+            health.curHealth = health.maxHealth;
+            attacked = false;
+            check = false;
+        }
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Damage"))
+        {
+            //health.curHealth -= 25;
+            attacked = true;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Heal"))
+        {
+            health.curHealth += Time.deltaTime * 5;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Checkpoint"))
+        {
+            curCheckpoint = other.transform;
+        }
+    }
+}
