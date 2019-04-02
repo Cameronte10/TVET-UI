9602e409647e50402845bb0d0ca7631b60fcd2b0
Cameronte10 <cjbaker30@yahoo.com.au>
2019-04-02 12:22:36 +1100

    djdnd

---
+++ AHHHHHH/Assets/Scripts/Boundary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Boundary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
